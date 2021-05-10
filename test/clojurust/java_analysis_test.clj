d31ee0b0f751645eeb9ffc99df24d92b6eac1066
Ivan Pierre <me@ivanpierre.world>
2021-05-10 03:17:18 +0200

    add project files

---
+++ test/clojurust/java_analysis_test.clj
@@ -0,0 +1,7 @@
+(ns clojurust.java-analysis-test
+  (:require [clojure.test :refer :all]
+            [clojurust.java-analysis :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
