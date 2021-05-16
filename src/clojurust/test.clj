5b9b81ae2381b085227d61c17853207f7c3945e0
Ivan Pierre <me@ivanpierre.world>
2021-05-16 19:26:07 +0200

    Test

--- src/clojurust/test.clj
+++ src/clojurust/test.clj
@@ -1,7 +1,40 @@
-(ns clojurust.test
-  (:require [clojure.reflect :as ref]
-            [clojure.pprint :as p]))
+(ns clojurust.test)
 
-(p/pprint (ref/type-reflect java.lang.reflect.Field))
+(require '[clojure.reflect :as r])
+(require '[clojure.pprint :as p])
+;;; (import org.reflections.reflections Reflection)
 
-(.getDeclaredFields (class java.lang.reflect.Field))
+(comment (p/pprint (r/reflect java.lang.reflect.Field)))
+
+(defn p [obj] (p/pprint obj))
+(defn pt [obj] (p/print-table obj))
+
+(def xref {:interface {}
+           :abstract {}
+           :class {}})
+
+(defn pobj [obj]
+  (println "Name")
+  (p (.getName (class obj)))
+  (println "Fields")
+  (p (.getDeclaredFields (obj)))
+  (println "Functions")
+  (p (.toGenericString (obj))))
+
+(defn po [obj]
+  {obj
+   (r/reflect obj)})
+
+
+;;(println "-----------------------------")
+;;(println "Object")
+;;(println "------------------------")
+;;(println (p (po Object)))
+(p (po java.lang.reflect.AnnotatedElement))
+;;(println "------------------------")
+
+(p (all-ns))
+;;; (Reflection. "java.lang")
+
+;;; (doseq [p (.getURLs (java.lang.ClassLoader/getSystemClassLoader))] (println (.getPath p)))
+(p (loaded-libs))
